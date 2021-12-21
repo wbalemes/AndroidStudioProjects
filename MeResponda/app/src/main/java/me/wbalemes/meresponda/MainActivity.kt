16e8e092add07ab8472e08c4288a3b991a76054e
Warley Lemes <90565691+wbalemes@users.noreply.github.com>
2021-12-21 20:55:23 -0300

    first release
    final version  apk Answer me

--- MeResponda/app/src/main/java/me/wbalemes/meresponda/MainActivity.kt
+++ MeResponda/app/src/main/java/me/wbalemes/meresponda/MainActivity.kt
@@ -2,19 +2,34 @@ package me.wbalemes.meresponda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    val res = arrayOf("sim", "Não conte com isso!!", "Talvez")
-
+    val res = arrayOf("sim", "Não conte com isso!!", "Talvez", "Com certeza!")
+    val random = Random.Default
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnClick = findViewById(R.id.btnClick)
-        btnClick.setOnClickListener {
-            textView = res[0]
+
+        val btnEnviar = findViewById<Button>(R.id.btnEnviar)
+        val resposta = findViewById<TextView>(R.id.resposta)
+        val pergunta = findViewById<EditText>(R.id.lblPergunta)
+
+
+        // Register the onClick listener with the implementation above
+        btnEnviar.setOnClickListener {
+            if (pergunta.text.isEmpty()) {
+                resposta.text = "Faça uma pergunta."
+            } else {
+                val i = random.nextInt(4)
+                resposta.text = res[i]
+            }
         }
     }
 }
